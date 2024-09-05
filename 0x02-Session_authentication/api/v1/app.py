dba98adcfb9744c000af7632c5cfd7486fd46d59
Abraham Livinus <abrahamlivinus@gmail.com>
2024-09-05 08:44:44 -0700

    api/v1/views/users.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,78 +12,65 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-# Define auth variable and initialize to None
 auth = None
-# Get the authentication type from environment variable
-AUTH_TYPE = os.getenv("AUTH_TYPE")
-
-# Import appropriate authentication mechanism based on AUTH_TYPE
-if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+if os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
+elif os.getenv("AUTH_TYPE") == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
+elif os.getenv("AUTH_TYPE") == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
+elif os.getenv('AUTH_TYPE') == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
-def bef_req():
-    """
-    Filter each request before it's handled by the proper route
-    """
+def before_request_func():
+    """ before request function """
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
+        return None
+    if auth.require_auth(request.path, excluded_paths):
         pass
     else:
-        # Attach the current user to the request if it exists
-        setattr(request, "current_user", auth.current_user(request))
-        
-        # List of endpoints excluded from authentication
-        excluded = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
-        ]
-        
-        # If authentication is required for the requested path
-        if auth.require_auth(request.path, excluded):
-            cookie = auth.session_cookie(request)  # Get session cookie from request
-            if auth.authorization_header(request) is None and cookie is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
+        return None
+    if (not auth.authorization_header(request)
+            and not auth.session_cookie(request)):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Request unauthorized handler """
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Request forbidden handler """
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
-    # Set the host and port from environment variables or default to 0.0.0.0 and 5000
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
